7b7d25ba289ff91d321ecc350d2963295cf6b78b
smlab <neet_kun_3342@yahoo.co.jp>
2022-04-02 17:18:08 +0900

    sassa04 DataScriptsの設定

---
+++ Scripts/DataManage/DataScripts.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DataScripts : ScriptableObject
+{
+    //ログインで使用する
+    public static string SchoolNumber;      //学校番号を保持する
+    public static int SchoolGrade;          //生徒の学年を保持する
+    public static int SchoolClass;          //生徒のクラスを保持する
+    public static int PersonalNumber;       //生徒の出席番号を保持する
+    public static int StudentGender;        //生徒の性別を保持する
+    public static int StudentAge;           //生徒の年齢を保持する
+
+    //ゲーム開始してから使用する変数（※ゲームごとに初期化が必要）
+    public static int pattern;              //ゲームの種類を保持
+    public static string gamedata;          //XML形式でゲームデータを保持する
+}
---
+++ Scripts/ServerCommunication/ServerTest.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+using System;
+using System.Net.Http;
+
+public class ServerTest : MonoBehaviour
+{
+    public Text textMesh;
+    public UnityEvent OnCompleteAction; //インスペクタから登録できる
+
+    void Start()
+    {
+        DemoApplication demoApp = new DemoApplication();
+        demoApp.CompleteHandler += UpdateTextCallback;
+        demoApp.GetHtmlAsync();
+    }
+
+    //GetHtmlAsyncの処理が終了した後に呼び出される関数。Actionによって追加されている。
+    void UpdateTextCallback(string html)
+    {
+        textMesh.text = html;
+        OnCompleteAction?.Invoke(); //インスペクタで登録した処理が実行される
+    }
+
+    //Inspectorで登録してあるとUpdateTextCallbackから呼び出される
+    public void DebugLogOK()
+    {
+        Debug.Log("OK");
+    }
+}
+
+//DemoViewから呼び出されるクラス
+public class DemoApplication
+{
+    private string url = "https://star2020.xsrv.jp/app_assessment/unityTest.php/?SchoolNumber=s2&SchoolGrade=2&SchoolClass=2&PersonalNumber=23&StudentGender=1&StudentAge=14&pattern=0&data1=2";
+
+    //public delegate void OnCompleteDelegate(string html);
+    //public event OnCompleteDelegate CompleteHandler;
+    public event Action<string> CompleteHandler; //上の２行を書き換えた
+
+    public async void GetHtmlAsync()
+    {
+        HttpClient client = new HttpClient();
+        var result = await client.GetStringAsync(url);
+        CompleteHandler?.Invoke(result);
+    }
+}
